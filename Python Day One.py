d0ea52fdca851e92f5efb0a7e3dd0985422e4305
markimoo999 <sazzimatan@yahoo.com>
2017-07-03 15:38:32 -0400

    Add files via upload

---
+++ Python Day One.py
@@ -0,0 +1,21 @@
+print "Wanna play Rock Paper Scissors? Let's Play!"
+your_pick=raw_input("Your Choice\n")
+import random
+my_pick=random.randint(0,2)
+if my_pick==0:
+    print "My Choice is Rock!"
+elif my_pick==1:
+    print "My Choice is Paper!"
+else:
+    print "My Choice is Scissors!"
+if my_pick==0 and your_pick==Scissors
+    print "I Win!"
+elif my_pick==0 and your_pick==Paper
+    print "You Win!"
+elif my_pick==0 and your_pick==Rock
+    print "Tie!"
+elif my_pick==1 and your_pick==Rock
+    print "I Win!"
+
+
+
